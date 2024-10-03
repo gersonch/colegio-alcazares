1719e5064b32d9e10230f335ab0b61c488811e57
gersonch <gersonch@users.noreply.github.com>
2024-10-03 16:53:49 -0300

    arreglar rutas img

--- src/components/info/datosCarousel.js
+++ src/components/info/datosCarousel.js
@@ -2,19 +2,19 @@ export const info1 = {
   h: "Simulacro Regional de Sismo",
   p: "El día de hoy 03 de octubre, se llevó  a cabo en nuestro Establecimiento el Simulacro Regional de Sismo, en donde se puso en práctica nuestro Plan Integral de Seguridad Escolar.",
   href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320842366",
-  img: "../public/sismo.webp",
+  img: "/sismo.webp",
 };
 
 export const info2 = {
   h: "Priemiacion Acto Fiestas Patrias",
   p: "Se llevó a cabo la premiación por la decoración de salas, el conteo de huasos, y la elección de las parejas ganadoras del concurso de Cueca.",
   href: "https://www.flickr.com/photos/200544313@N07/albums/",
-  img: "../public/acto-fiestas.webp",
+  img: "/acto-fiestas.webp",
 };
 
 export const info3 = {
   h: "Gala Fiestas Patrias",
   p: "Vivimos un hermoso momento en que nuestros queridos alumnos nos deleitaron con sus bailes folclóricos.",
   href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320559952/",
-  img: "../public/gala.webp",
+  img: "/gala.webp",
 };
