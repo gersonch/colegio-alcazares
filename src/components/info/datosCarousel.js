f2be8411aa3a78bcebc78e94a3af7fa19e3c9343
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-10-10 19:37:33 -0300

    mostrar carousel dinamicamente

--- src/components/info/datosCarousel.js
+++ src/components/info/datosCarousel.js
@@ -1,21 +1,20 @@
-export const info1 = {
-  h: "Dia del Asistente",
-  p: "El pasado viernes disfrutamos de un momento muy especial durante la celebración del Día del Asistente. Fue una jornada llena de alegría y reconocimiento, donde compartimos gratos instantes que reflejaron el aprecio y la dedicación de todos los asistentes.",
-  href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320986941",
-  img: "/dia-del-asistente.webp",
-};
-
-
-export const info2 = {
-  h: "Simulacro Regional de Sismo",
-  p: "El día de hoy 03 de octubre, se llevó  a cabo en nuestro Establecimiento el Simulacro Regional de Sismo, en donde se puso en práctica nuestro Plan Integral de Seguridad Escolar.",
-  href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320842366",
-  img: "/sismo.webp",
-};
-
-export const info3 = {
-  h: "Priemiacion Acto Fiestas Patrias",
-  p: "Se llevó a cabo la premiación por la decoración de salas, el conteo de huasos, y la elección de las parejas ganadoras del concurso de Cueca.",
-  href: "https://www.flickr.com/photos/200544313@N07/albums/",
-  img: "/acto-fiestas.webp",
-};
+export const carouselData = [
+  {
+    h: "Dia del Asistente",
+    p: "El pasado viernes disfrutamos de un momento muy especial durante la celebración del Día del Asistente. Fue una jornada llena de alegría y reconocimiento, donde compartimos gratos instantes que reflejaron el aprecio y la dedicación de todos los asistentes.",
+    href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320986941",
+    img: "/dia-del-asistente.webp",
+  },
+  {
+    h: "Simulacro Regional de Sismo",
+    p: "El día de hoy 03 de octubre, se llevó  a cabo en nuestro Establecimiento el Simulacro Regional de Sismo, en donde se puso en práctica nuestro Plan Integral de Seguridad Escolar.",
+    href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320842366",
+    img: "/sismo.webp",
+  },
+  {
+    h: "Premiación Acto Fiestas Patrias",
+    p: "Se llevó a cabo la premiación por la decoración de salas, el conteo de huasos, y la elección de las parejas ganadoras del concurso de Cueca.",
+    href: "https://www.flickr.com/photos/200544313@N07/albums/",
+    img: "/acto-fiestas.webp",
+  },
+];
