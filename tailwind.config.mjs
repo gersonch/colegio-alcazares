ac1e2d6d64bd9ff27f4313bb74747f1596d5f24c
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-10-11 10:17:52 -0300

    optimizar fuente

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,11 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        pacifico: ['Pacifico', 'cursive'],
+      }
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
