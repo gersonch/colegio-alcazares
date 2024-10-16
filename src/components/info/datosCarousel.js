5b00b98287226d77c569256471f77a6201008c96
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-10-16 12:15:24 -0300

    expo segundo

--- src/components/info/datosCarousel.js
+++ src/components/info/datosCarousel.js
@@ -1,5 +1,11 @@
 export const carouselData = [
   {
+    h: "Exposición Segundo Básico",
+    p: "Los estudiantes de segundo básico finalizaron la unidad sobre pueblos originarios creando maquetas que representaban aspectos clave de distintas culturas indígenas.  Durante el proceso, los niños demostraron gran creatividad y trabajo en equipo, investigando y aprendiendo sobre las costumbres y tradiciones de las comunidades. La actividad permitió a los niños aprender de forma creativa y compartir sus conocimientos en una exposición final.",
+    href: "https://www.flickr.com/photos/200544313@N07/albums/72177720321277569/",
+    img: "/expo-segundo.webp",
+  },
+  {
     h: "Dia del Asistente",
     p: "El pasado viernes disfrutamos de un momento muy especial durante la celebración del Día del Asistente. Fue una jornada llena de alegría y reconocimiento, donde compartimos gratos instantes que reflejaron el aprecio y la dedicación de todos los asistentes.",
     href: "https://www.flickr.com/photos/200544313@N07/albums/72177720320986941",
