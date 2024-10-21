8770b114443eb4519e47c536cb86950a1fdf0dde
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-10-21 10:57:00 -0300

    dia salud mental

--- src/components/info/datosCarousel.js
+++ src/components/info/datosCarousel.js
@@ -1,7 +1,13 @@
 export const carouselData = [
   {
+    h: "Dia de la Salud Mental",
+    p: "Los estudiantes de segundo básico finalizaron la unidad sobre pueblos originarios creando maquetas que representaban aspectos clave de distintas culturas indígenas.  Durante el proceso, los niños demostraron gran creatividad y trabajo en equipo, investigando y aprendiendo sobre las costumbres y tradiciones de las comunidades.",
+    href: "https://www.flickr.com/photos/200544313@N07/albums/72177720321284545/",
+    img: "/dia-salud-mental.webp",
+  },
+  {
     h: "Exposición Segundo Básico",
-    p: "Los estudiantes de segundo básico finalizaron la unidad sobre pueblos originarios creando maquetas que representaban aspectos clave de distintas culturas indígenas.  Durante el proceso, los niños demostraron gran creatividad y trabajo en equipo, investigando y aprendiendo sobre las costumbres y tradiciones de las comunidades. La actividad permitió a los niños aprender de forma creativa y compartir sus conocimientos en una exposición final.",
+    p: "Celebramos el Día de la Salud Mental en nuestro colegio creando el Árbol de la Salud Mental. Los estudiantes en compañía de la dupla psicosocial participaron escribiendo en hojas sus experiencias y acciones que les ayudan a sentirse mejor y a cuidar su bienestar emocional",
     href: "https://www.flickr.com/photos/200544313@N07/albums/72177720321277569/",
     img: "/expo-segundo.webp",
   },
